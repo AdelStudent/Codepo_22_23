82efca5dfedd658440cee12c11fb2425bede0ace
AdelNehili <adel@LAPTOP-435R1L8O>
2023-07-27 10:04:04 +0100

    Update

--- bash_script.sh
+++ bash_script.sh
@@ -4,16 +4,21 @@
 # username
 # password
 
-FILE="C:/Users/Sandra/OneDrive_EPHEC_asbl/Bureau/gitPass.txt" # replace with the actual path to your file
+#FILE="C:\Users\Sandra\OneDrive_EPHEC_asbl\Bureau\gitPass.txt" # replace with the actual path to your file
+FILE="..\..\Mes projets\gitPass.txt"
 
 USERNAME=$(sed -n '1p' < $FILE)
 PASSWORD=$(sed -n '2p' < $FILE)
 
 git status
 git pull
+sleep 1
+
 git add .
 git commit -a -m "Update"
 git push
+sleep 1
+
 echo $USERNAME
 sleep 1 # wait for 1 second
 echo $PASSWORD
