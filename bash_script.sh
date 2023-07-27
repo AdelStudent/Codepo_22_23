d99be0e1616d82e3bd55d896f75785c04c984fab
AdelNehili <adel@LAPTOP-435R1L8O>
2023-07-27 10:18:52 +0100

    Update

--- bash_script.sh
+++ bash_script.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+FILE='../../Mes_projets/gitPassword.txt'
+
 # assuming the file is in format:
 # username
 # password
+#C:\Users\Sandra\OneDrive_EPHEC_asbl\Bureau\Polytech\Mes_projets\gitP
+while IFS= read -r line
+do
+  if [ -z "$USERNAME" ]
+  then
+    USERNAME="$line"
+  else
+    PASSWORD="$line"
+  fi
+done < "$FILE"
 
-#FILE="C:\Users\Sandra\OneDrive_EPHEC_asbl\Bureau\gitPass.txt" # replace with the actual path to your file
-FILE="..\..\Mes projets\gitPass.txt"
 
 USERNAME=$(sed -n '1p' < $FILE)
 PASSWORD=$(sed -n '2p' < $FILE)
