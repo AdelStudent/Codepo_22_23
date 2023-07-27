935dc7292be671316f4848a860ba22f918087c4b
AdelNehili <49352113+AdelNehili@users.noreply.github.com>
2023-07-27 12:34:16 +0200

    Add files via upload

---
+++ Modélisation Congo/main.py
@@ -0,0 +1,58 @@
+# Modélisation du Système de la Cameskin
+
+from powersourcesClass import powersources # import la class en lui-même 
+from TimeManagerClass import TimeManager
+from powerconsummerClass import powerconsummer
+from BatteryClass import Battery
+
+ON = 1 #Pris en compte 
+OFF = 0 #Pas pris en compte 
+
+# Sources 
+PV_behavior = [0,0,0,0,0,0,0.104102729,0.35242141,0.642178054,0.763581929,0.998803315,0.865229468,1,0.873200005,0.63030394,0.334391656,0.097306797,0,0,0,0,0,0,0] #Pourcent du max de la journée 
+max_pv_power = 2264.59 # Puissance maximale enregistré (PVGis) pour une journée de Juillet 
+PV = powersources("PV", ON*max_pv_power, PV_behavior) 
+
+Grid_behavior = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+Grid = powersources("Réseau", ON*10000, Grid_behavior)
+# 10 000 W : Puissance du réseau
+GroupElec_behavior = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+GroupElec = powersources("Groupe Electrogène", OFF*1400, GroupElec_behavior)
+# 1 400 W : Puissance prise au hasard pour la production électrogène (climatiseur)
+
+# Battery 
+my_Battery = Battery("Battery",50, 20) # SOC initial = 50%, nombre de batterie = 20
+
+# Consummer 
+nbr_lumiere = 85
+nbr_ordi = 10
+nbr_impr = 10
+nbr_clim_bureau = 9
+nbr_clim_medoc = 45
+
+Lumiere_behavior = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]
+Lumiere = powerconsummer("Lumière", ON*nbr_lumiere*40, Lumiere_behavior)
+# 40W -> Puissance consommée par une lumière
+Ordinateur_behavior = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]
+Ordinateur = powerconsummer("Ordinateur", ON*nbr_ordi*35, Ordinateur_behavior)
+# 35W -> Puissance consommée par un ordinateur 
+Imprimante_behavior = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]
+Imprimante = powerconsummer("Imprimante", ON*nbr_impr*30, Imprimante_behavior)
+# 30W -> Puissance consommée par une imprimante 
+
+Clim_bureau_behavior = [0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0]
+Climatiseur_bureau = powerconsummer("Climatiseur Bureau", OFF*0.293*(8*18000 + 1*24000)/24, Clim_bureau_behavior) # 9 Climatiseurs dans le batiment administratif 
+# 8*18000+1*24000 
+# 1 BTU = 2,93 10^-4 
+
+Clim_medoc_behavior = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+Climatiseur_medoc = powerconsummer("Climatiseur Medoc", OFF*(14*24000 + 18000*37)/24*0.293, Clim_medoc_behavior) # 51 Climatiseurs dans les dépôts
+# 14*24000 + 37*18000
+
+# Time 
+Obj = [PV, Grid, GroupElec, my_Battery, Lumiere, Ordinateur, Imprimante,Climatiseur_bureau,Climatiseur_medoc]
+TimeSync = TimeManager(0, Obj) # Doit prendre connaissance de tout le monde -> après les autres objets 
+
+# Graphe
+nbr_jour = 1
+TimeSync.live_hours(nbr_jour*24) 
